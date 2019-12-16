16324c7a93fa7b2e207829407baf417e73e5ce21
Emre Kırcı <Emrekrci@hotmail.com>
2019-12-17 01:03:05 +0300

    Customer Controller and CustomerManage view were added.

---
+++ TurkTicaretCRM.TT.Mvc/Controllers/CustomerController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TurkTicaretCRM.TT.Bussiness.Abstract;
+
+namespace TurkTicaretCRM.TT.Mvc.Controllers
+{
+    public class CustomerController : Controller
+    {
+        ICustomerService _customerService;
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CustomerManage()
+        {
+            return View();
+        }
+
+        public ActionResult GetAllCustomers()
+        {
+            var result = _customerService.GetAll();
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
